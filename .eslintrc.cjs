5f14851dbfa6926eb71a87761613315018bf0a00
ngseke <ngseke@gmail.com>
2023-07-26 21:19:54 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: 'standard',
+  rules: {
+    'no-unused-vars': 'warn',
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'only-multiline',
+    }],
+    'prefer-const': 0,
+  },
+}
